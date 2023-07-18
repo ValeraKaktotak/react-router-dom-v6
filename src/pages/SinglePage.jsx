6f1cf43361a43c3acfe87928bc812d330a37c4d2
ValeraKaktotak <valerakaktotak@gmail.com>
2023-07-18 20:54:26 +0300

    useNavigate

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 export const SinglePage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [post, setPosts] = useState(null);
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -10,15 +11,18 @@ export const SinglePage = () => {
       .then((data) => setPosts(data));
   }, []);
 
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div>
-        <h1>
-            {post? post.title: ''}
-        </h1>
-        <p>
-            {post?post.body:''}
-        </p>
-        <Link to={`/posts/${id}/edit`} >Edit post</Link>
+      <h1>{post ? post.title : ""}</h1>
+      <p>{post ? post.body : ""}</p>
+      <Link to={`/posts/${id}/edit`}>Edit post</Link>
+      <div>
+        <button onClick={goBack}>Go Back</button>
+      </div>
     </div>
   );
 };
